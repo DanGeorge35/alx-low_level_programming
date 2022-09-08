2307e0da26f289e023dd97595f7c8b6fe82af849
Dan George <dangeorge35@gmail.com>
2022-09-08 10:45:54 -0700

    C PROGRAM

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /*
- *  Program that prints
+ *  Programming is like building a multilingual puzzle
  */
-
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
