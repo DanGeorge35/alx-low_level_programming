3080abfe3acf9f32ba36436fd5763ec5d6b4fc17
Dan George <dangeorge35@gmail.com>
2022-09-08 11:37:11 -0700

    C PROGRAM

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,18 +7,10 @@
  */
 int main(void)
 {
-int Size;
-Size = sizeof(char);	
-printf("Size of a char: %d byte(s)\n", Size);
-Size = sizeof(int);
-printf("Size of a int: %d byte(s)\n", Size);
-Size = sizeof(long int);
-printf("Size of a long int: %d byte(s)\n", Size);
-Size = sizeof(long long int);
-printf("Size of a long long int: %d byte(s)\n", Size);
-Size = sizeof(float);
-printf("Size of a float: %d byte(s)\n", Size);
-
-
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
