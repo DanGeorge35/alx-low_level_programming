cfbe1d67ee62f1ff054fe8b48102f5ef445ddae5
Dan George <dangeorge35@gmail.com>
2022-09-09 15:56:13 -0700

    C PROGRAM

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,12 +4,10 @@
  * main - main block
  * Description: Numbers must be separated by ,, followed by a space
  * The three digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
  * Print only the smallest combination of three digits
  * Numbers should be printed in ascending order, with three digits
- * Only use the putchar function (every other function (printf, puts, etc…) is forbidden)
  * Only use putchar six times maximum in your code
-All your code should be in the main function
+ * All your code should be in the main function
  * Return: 0
  */
 int main(void)
