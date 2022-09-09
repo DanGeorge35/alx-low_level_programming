27a5719c407ee39e67f1b37c73ccf51cc07d0b36
Dan George <dangeorge35@gmail.com>
2022-09-09 15:30:46 -0700

    C PROGRAM

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Number starting from 0, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+
+	return (0);
+}
