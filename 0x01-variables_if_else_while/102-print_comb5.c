bb2a8df30c0a64c1e08e6d580fb1b932b5b91cc0
Dan George <dangeorge35@gmail.com>
2022-09-09 17:16:23 -0700

    C PROGRAM

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,6 @@
  */
 int main(void)
 {
-	int c;
 	int d;
 	int e = 0;
 
@@ -19,24 +18,17 @@ int main(void)
 		d = 0;
 		while (d < 10)
 		{
-			c = 0;
-			while (c < 10)
-			{
-				if (c != d && d != e && e < d && d < c)
+				if (d != e && e < d)
 				{
 					putchar('0' + e);
 					putchar('0' + d);
-					putchar('0' + c);
 
-					if (c + d + e != 9 + 8 + 7)
+					if (d + e != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-
-				c++;
-			}
 			d++;
 		}
 		e++;
