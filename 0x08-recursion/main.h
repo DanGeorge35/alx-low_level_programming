2ee11d3050f23a31bbf9ef247f86050a41fa9c53
Dan George <dangeorge35@gmail.com>
2022-09-29 21:50:19 -0700

    C PROGRAM

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,4 +12,3 @@ int last_index(char *s);
 int check(char *s, int start, int end, int pair);
 int is_palindrome(char *s);
 int checker(char *s1, char *s2, int a, int b, int wildUsed);
-int wildcmp(char *s1, char *s2);
